db85ff19b47bd975fdd3396abf8243d6dbb6e5c2
john-sawicki <3Gxapnil>
2018-05-14 22:32:04 -0500

    working after step two

--- RedneckJokes/src/main/java/com/example/android/redneckJokes/RedneckJokes.java
+++ RedneckJokes/src/main/java/com/example/android/redneckJokes/RedneckJokes.java
@@ -2,7 +2,7 @@ package com.example.android.redneckJokes;
 
 public class RedneckJokes {
     public String[] redneckJokes={
-            " What's The Difference Between Helen Keller And A Redneck?"+'\n'+"She got famous for not being able to read. ",
+            "What's The Difference Between Helen Keller And A Redneck?"+'\n'+"She got famous for not being able to read. ",
             "Why Do Ducks Fly Upside Down Over Trailer Parks?"+'\n'+"There's nothing worth crapping on.",
             "What Do Rednecks Call Duct Tape?"+'\n'+"Chrome",
             "What Can A Pizza Do That A Redneck Can't?"+'\n'+"Feed a family of 4."
--- steptwojokes/src/main/java/com/example/android/steptwojokes/StepTwoMainActivity.java
+++ steptwojokes/src/main/java/com/example/android/steptwojokes/StepTwoMainActivity.java
@@ -3,17 +3,33 @@ package com.example.android.steptwojokes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class StepTwoMainActivity extends AppCompatActivity {
     private String[] redneckJokes;
+    private String redneckJoke;
+    private int jokeCount = 0;
+    Button tellJokeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_two_main);
+        tellJokeButton = findViewById(R.id.goToStepTwoButton);
         Bundle extras = getIntent().getExtras();
         if(extras!=null){
             redneckJokes = extras.getStringArray("jokes");
             Log.d("Step two activity", redneckJokes[0]);
         }
+        tellJokeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                redneckJoke = redneckJokes[jokeCount];
+                Toast.makeText(getApplicationContext(), redneckJoke, Toast.LENGTH_SHORT).show();
+                jokeCount+=1;
+                if(jokeCount==redneckJokes.length) jokeCount=0;    //return to the first joke
+            }
+        });
     }
 }
